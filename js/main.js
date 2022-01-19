a766b1512c5bf6203ea64dde43e28976f5b5917a
Fadziso Gabrielle <fadziso@novodigital.biz>
2022-01-19 20:33:57 +0200

    Modified

--- js/main.js
+++ js/main.js
@@ -1,7 +1,13 @@
 // get current year instance
-
 const currentYear = new Date().getFullYear();
 
 const year = document.getElementById('year');
 
 year.innerHTML = currentYear + '';
+
+let loader = document.getElementById("preloader");
+
+// page loader function
+setTimeout(window.addEventListener('load', () => {
+    loader.style.display = "none";
+}), 3000)
